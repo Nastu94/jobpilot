bd34beb1d4ea0777bf386f003d032fd221ea694d
Nastu94 <vittoriosoligo94@hotmail.it>
2026-07-07 11:52:46 +0200

    feat: add sector model

---
+++ app/Models/Sector.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Sector extends Model
+{
+    protected $guarded = [];
+
+    public function profiles(): BelongsToMany
+    {
+        return $this->belongsToMany(Profile::class)->withTimestamps();
+    }
+}
