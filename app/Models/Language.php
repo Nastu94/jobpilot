60836e4e0514875e1baca2f7adbbce986b10a2cb
Nastu94 <vittoriosoligo94@hotmail.it>
2026-07-07 12:37:05 +0200

    feat: add language model

---
+++ app/Models/Language.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Language extends Model
+{
+    protected $fillable = [
+        'name',
+        'code',
+    ];
+
+    public function profiles(): BelongsToMany
+    {
+        return $this->belongsToMany(Profile::class)
+            ->withPivot([
+                'proficiency_level',
+                'is_native',
+                'notes',
+            ])
+            ->withTimestamps();
+    }
+}
