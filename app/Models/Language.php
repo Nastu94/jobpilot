7cabfc01122eee106ab8222585393d7733c4af7d
Nastu94 <vittoriosoligo94@hotmail.it>
2026-07-07 17:09:39 +0200

    feat: add language job requirements relation

--- app/Models/Language.php
+++ app/Models/Language.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Language extends Model
 {
@@ -21,5 +22,10 @@ class Language extends Model
                 'notes',
             ])
             ->withTimestamps();
+    }
+
+    public function jobPostingRequirements(): HasMany
+    {
+        return $this->hasMany(JobPostingRequirement::class)->orderBy('position');
     }
 }
